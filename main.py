0e3345507d3daf63749871b35c7127de536c6c70
andrejmaznin <andrejmaznin@gmail.com>
2022-03-13 22:35:58 +0500

    initial

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('main_page.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
