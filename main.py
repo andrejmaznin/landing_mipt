5dffe1a66a480ca6a17051434269e7195e4bdb89
andrejmaznin <andrejmaznin@gmail.com>
2022-03-15 10:22:51 +0500

    fix

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def main():
     return render_template('main_page.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
